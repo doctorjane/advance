c80fd9813db0f731fe58ce340c3ca78ad9d33007
janemacfarlane <jfmacfarlane@lbl.gov>
2019-01-19 09:53:50 -0800

    fix csv_select_nh

--- bin/csv_select_nh.rb
+++ bin/csv_select_nh.rb
@@ -1,21 +1,13 @@
 #!/usr/bin/env ruby
 require 'csv'
-# $stderr.puts "#{__FILE__}:#{__LINE__}"
 
 test_proc = eval "lambda {|row| #{ARGV.shift}}"
 
-input = CSV.new(ARGF, :headers => true, :return_headers => true, :converters => :numeric)
-output = CSV.new($stdout, :headers => true, :write_headers => true)
+input = CSV.new(ARGF, :converters => :numeric)
+output = CSV.new($stdout)
 
-input.each.with_index do |row, index|
-  # $stderr.puts "#{index}: >>#{row.to_s.chomp}<<"
-  if row.header_row?
-    output << row
-    next
-  end
-
+input.each do |row|
   if test_proc.call(row)
     output << row
-    next
   end
 end
---
+++ test/csv_select_nh_test.rb
@@ -0,0 +1,27 @@
+require 'minitest/autorun'
+
+describe "csv_select_nh" do
+  it "filters csv rows based on the ruby expression" do
+    tmp_file = Tempfile.new('csv_file')
+    begin
+      csv = <<CSV
+abc,111,555
+abc,222,666
+CSV
+      tmp_file.write(csv)
+      tmp_file.rewind
+
+      expected = <<CSV
+abc,222,666
+CSV
+      csv_select_bin = File.expand_path("./bin/csv_select_nh.rb")
+      output = `#{csv_select_bin} "row[2]==666" < #{tmp_file.path}`
+
+      output.must_equal(expected)
+
+    ensure
+      tmp_file.close
+      tmp_file.unlink
+    end
+  end
+end
