7b658923a6b4e949857d8a65ed20d5a548466e1f
Jane Macfarlane <jane@doctorjane.org>
2020-02-23 08:18:26 -0800

    correct split_csv output file names

--- bin/split_csv.rb
+++ bin/split_csv.rb
@@ -9,13 +9,15 @@ end
 csv_file = ARGV[0]
 lines = ARGV[1]
 csv_file_name = File.basename(csv_file)
-system "head -n 1 #{csv_file} > #{csv_file_name}_header"
+header_file_name = "#{csv_file_name}_header"
+system "head -n 1 #{csv_file} > #{header_file_name}"
 system "tail -n +2 #{csv_file} | split -l #{lines} -a 3 - #{csv_file_name}_"
-files = Dir.entries(".").reject { |f| f =~ %r{^(\.\.?|log)$} }
+files = Dir.entries(".").reject { |f| f.end_with?(header_file_name) || f =~ %r{^(\.\.?|log)$} }
 TeamEffort.work(files, 1) do |file|
   tmp_file = "tmp_#{file}"
-  do_cmd "cat #{csv_file_name}_header #{file} >> #{tmp_file}"
+  do_cmd "cat #{header_file_name} #{file} >> #{tmp_file}"
   do_cmd "mv #{tmp_file} #{file}"
 end
-do_cmd "rm #{csv_file_name}_header"
+do_cmd "rm #{header_file_name}"
+do_cmd "rename 's/^(.*)\.csv_(.*)$/$1_$2.csv/' *csv*"
 puts ""
--- lib/advance/version.rb
+++ lib/advance/version.rb
@@ -1,3 +1,3 @@
 module Advance
-  VERSION = "0.4.3"
+  VERSION = "0.4.4"
 end
